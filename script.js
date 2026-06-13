0a769c19d97dee189f5d4ed485e861f569275530
Aapoho666 <kunnariaapo196@gmail.com>
2026-06-13 22:25:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function startGame(){
+    alert("Peli alkaa! (Seuraava vaihe tulee myöhemmin)");
+}
 
+function settings(){
+    alert("Asetukset tulevat myöhemmin");
+}
+
+function quitGame(){
+    alert("Kiitos pelaamisesta!");
+}
